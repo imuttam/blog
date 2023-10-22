f869b10be471d50fe60aa5c6062d2321a9d0fcfc
imuttam <uttam.uk@gmail.com>
2023-10-22 20:01:05 +0530

    commit again on 22.10.2023

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import Post 
+from django.core.paginator import Paginator, EmptyPage
 
 def post_list(request):
-  posts = Post.published.all()
+  post_list = Post.published.all()
+  paginator = Paginator(post_list,2)
+  page_number = request.GET.get('page',1)
+  
+  try:
+    posts = paginator.page(page_number)
+  except EmptyPage:
+    posts = paginator.page(paginator.num_pages)    
+  
   return render(request, 'blog/list.html',{'posts':posts})
 
 
-def post_detail(request, id):
+def post_detail(request, year,month,day,post):
   post = get_object_or_404(Post, 
-                         id=id, status=Post.Status.PUBLISHED)
+                         status=Post.Status.PUBLISHED,
+                         slug=post,
+                         publish__year=year,
+                         publish__month=month,
+                         publish__day=day)
     
   return render(request, 'blog/detail.html', {'post':post})
