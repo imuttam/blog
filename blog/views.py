425e48d026e6814018c1f0030f8f320b6c472165
imuttam <uttam.uk@gmail.com>
2023-10-23 11:20:25 +0530

    first commit on 23.10.2023

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,29 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 
 from .models import Post 
-from django.core.paginator import Paginator, EmptyPage
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .forms import EmailPostForm
 
-def post_list(request):
-  post_list = Post.published.all()
-  paginator = Paginator(post_list,2)
-  page_number = request.GET.get('page',1)
+# def post_list(request):
+#   post_list = Post.published.all()
+#   paginator = Paginator(post_list,2)
+#   page_number = request.GET.get('page',1)
   
-  try:
-    posts = paginator.page(page_number)
-  except EmptyPage:
-    posts = paginator.page(paginator.num_pages)    
+#   try:
+#     posts = paginator.page(page_number)
+#   except EmptyPage:
+#     posts = paginator.page(paginator.num_pages) 
+#   except PageNotAnInteger:
+#     posts = paginator.page(1)    
   
-  return render(request, 'blog/list.html',{'posts':posts})
+#   return render(request, 'blog/list.html',{'posts':posts})
 
+class PostListView(ListView):
+  queryset = Post.published.all()
+  context_object_name = 'posts'
+  paginate_by = 2
+  template_name = 'blog/list.html'
 
 def post_detail(request, year,month,day,post):
   post = get_object_or_404(Post, 
@@ -25,3 +34,16 @@ def post_detail(request, year,month,day,post):
                          publish__day=day)
     
   return render(request, 'blog/detail.html', {'post':post})
+
+def post_share(request, post_id):
+  post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+  if request.method == 'POST':
+    form = EmailPostForm(request.POST)
+    
+    if form.is_valid():
+      cd = form.cleaned_data
+    else:
+      form = EmailPostForm()
+    return render(request, 'blog/share.html', 
+                  {'post':post, 'form':form})
+    
